fece03660cf0624dc4d4c4951c39a6fb81399005
TV73749 <v_TATO-88@MAIL.RU>
2024-02-26 19:42:30 +0300

    changed the name after pressing the button

---
+++ src/Leopold.js
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+export class Leopold extends Component  {
+    constructor(){
+        super();
+
+        this.state = {
+            Name: "Jack"
+        }
+    }
+    ChangeName =()=> {
+        this.setState({Name: "Leopold old old old"})
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Hello {this.state.Name}</h2>
+                <button onClick={()=>this.ChangeName()}>Click here for name change</button>
+            </div>
+        )
+    }
+}
